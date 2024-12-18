da06645da27f1d3c006882fd79b5bb81369c2297
Anusha2062 <pradhananusha1111@gmail.com>
2024-12-18 12:06:33 +0545

    Delete 3.php

--- 3.php
+++
@@ -1,7 +0,0 @@
-<?php
-function convertMinutesToSeconds($minutes) {
-    return $minutes * 60;
-}
-$minutes = 5; // Example input
-echo "\n$minutes minutes in seconds: " . convertMinutesToSeconds($minutes) . " seconds\n";
-?>
