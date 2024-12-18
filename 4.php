aa20db86e104133a79ba48161a00fe98ded8f886
Anusha2062 <pradhananusha1111@gmail.com>
2024-12-18 12:21:35 +0545

    Add files via upload

---
+++ 4.php
@@ -0,0 +1,9 @@
+
+<?php
+function addTwoNumbers($num1, $num2) {
+    return $num1 + $num2;
+}
+$num1 = 8; // Example inputs
+$num2 = 12;
+echo "\nThe sum of $num1 and $num2 is: " . addTwoNumbers($num1, $num2) . "\n";
+?>
