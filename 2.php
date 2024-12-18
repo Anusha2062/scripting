db0dd06ddf38572b335e59d2f662593cdc5a1d74
Anusha2062 <pradhananusha1111@gmail.com>
2024-12-18 11:41:40 +0545

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,6 @@
+<?php
+define("PI", 3.141592653589793); // Defining PI as a constant
+$radius = 5; // Example radius
+$areaOfCircle = PI * $radius * $radius;
+echo "\nThe area of a circle with radius $radius is: $areaOfCircle\n";
+?>
